aaf57576a7e640bc97528dea5bd6e63cbd32195c
Vladimir Novick <vnovick@gmail.com>
2016-08-02 16:08:29 +0300

    messing with routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
 /**
  * Created by vladimirn on 11/27/15.
  */
-import appHandler from './handlers/app-handler';
-import home from './controllers/homeController'
+import appHandler from '../handlers/app-handler';
+import home from '../controllers/homeController'
 export default (app) => {
     app.get('/', appHandler.bind(null, 'app'), home);
     app.get('/preview', appHandler.bind(null, 'preview'), home);
